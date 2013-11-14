d98defadac75547cc549553af3dcfa3e03ce4dab
manish999 <manishpathak.rg@gmail.com>
2013-11-14 12:52:09 +0530

    temporary commit.

--- src/com/rampgreen/caretakermobile/model/User.java
+++ src/com/rampgreen/caretakermobile/model/User.java
@@ -4,44 +4,60 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import android.widget.ImageView;
+
 import com.rampgreen.caretakermobile.R;
 import com.rampgreen.caretakermobile.interfaces.Populator;
-
-import android.widget.ImageView;
 
 import java.io.Serializable;
 import java.util.ArrayList;
 
 public class User implements Serializable, Populator
 {
-	private String name = "";
-
 	private boolean notification = false;
 	private ImageView imageUser = null;
 	private int notificationCount;
 	private boolean isOnDashboard = false;
 	private int imageResId = R.drawable.user;
-	private String Uid;
-	private String Username;
-	private String Gender;
-	private String DOB;
-	private String Emailid;
-	private String Registrationdate;
-	private String Height;
-	private String Weight;
-	private String Devicid;
+	private String uId;
+	private String userName;
+	private String gender;
+	private String dob;
+	private String eMailid;
+	private String registrationdate;
+	private String height;
+	private String weight;
+	private String devicid;
+	
+	private boolean userOnHomeScreen = false;
+
+	private boolean gsrNotification = false;
+	private boolean heartRateNotification = false;
+	private boolean accelerometerNotification = false;
+	private boolean tempratureNotification = false;
+	
+	private boolean gsrTextDisplay = false;
+	private boolean heartRateTextDisplay = false;
+	private boolean accelerometerTextDisplay = false;
+	private boolean tempratureTextDisplay = false;
+	
+	private boolean gsrVisualDisplay = false;
+	private boolean heartRateVisualDisplay = false;
+	private boolean accelerometerVisualDisplay = false;
+	private boolean tempratureVisualDisplay = false;
+	
 	private ArrayList<User> userList = new ArrayList<User>();
 
 	public User(String Uid,String Username,String Gender,String DOB,String Emailid,String Registrationdate,String Height,String Weight,String Devicid) {
-		this.Uid = Uid;
-		this.Username = Username;
-		this.Gender = Gender;
-		this.DOB = DOB;
-		this.Emailid = Emailid;
-		this.Registrationdate = Registrationdate;
-		this.Height = Height;
-		this.Weight = Weight;
-		this.Devicid = Devicid;
+		this.uId = Uid;
+		this.userName = Username;
+		this.gender = Gender;
+		this.dob = DOB;
+		this.eMailid = Emailid;
+		this.registrationdate = Registrationdate;
+		this.height = Height;
+		this.weight = Weight;
+		this.devicid = Devicid;
 	}
 
 	public User () {
@@ -53,23 +69,13 @@ public class User implements Serializable, Populator
 	 * @param imageUser
 	 * @param notificationCount
 	 */
-	public User(String name, boolean notification, ImageView imageUser,
+	public User(String userName, boolean notification, ImageView imageUser,
 			int notificationCount)
 	{
-		this.name = name;
+		this.userName = userName;
 		this.notification = notification;
 		this.imageUser = imageUser;
 		this.notificationCount = notificationCount;
-	}
-
-	public String getName()
-	{
-		return name;
-	}
-
-	public void setName(String name)
-	{
-		this.name = name;
 	}
 
 	public boolean isNotification()
@@ -124,92 +130,222 @@ public class User implements Serializable, Populator
 
 	public String getUid()
 	{
-		return Uid;
+		return uId;
 	}
 
 	public void setUid(String uid)
 	{
-		Uid = uid;
+		uId = uid;
 	}
 
 	public String getUsername()
 	{
-		return Username;
+		return userName;
 	}
 
 	public void setUsername(String username)
 	{
-		Username = username;
+		userName = username;
 	}
 
 	public String getGender()
 	{
-		return Gender;
+		return gender;
 	}
 
 	public void setGender(String gender)
 	{
-		Gender = gender;
+		this.gender = gender;
 	}
 
 	public String getDOB()
 	{
-		return DOB;
+		return dob;
 	}
 
 	public void setDOB(String dOB)
 	{
-		DOB = dOB;
+		dob = dOB;
 	}
 
 	public String getEmailid()
 	{
-		return Emailid;
+		return eMailid;
 	}
 
 	public void setEmailid(String emailid)
 	{
-		Emailid = emailid;
+		eMailid = emailid;
 	}
 
 	public String getRegistrationdate()
 	{
-		return Registrationdate;
+		return registrationdate;
 	}
 
 	public void setRegistrationdate(String registrationdate)
 	{
-		Registrationdate = registrationdate;
+		this.registrationdate = registrationdate;
 	}
 
 	public String getHeight()
 	{
-		return Height;
+		return height;
 	}
 
 	public void setHeight(String height)
 	{
-		Height = height;
+		this.height = height;
 	}
 
 	public String getWeight()
 	{
-		return Weight;
+		return weight;
 	}
 
 	public void setWeight(String weight)
 	{
-		Weight = weight;
+		this.weight = weight;
 	}
 
 	public String getDevicid()
 	{
-		return Devicid;
+		return devicid;
 	}
 
 	public void setDevicid(String devicid)
 	{
-		Devicid = devicid;
+		this.devicid = devicid;
+	}
+	
+	public boolean isUserOnHomeScreen()
+	{
+		return userOnHomeScreen;
+	}
+
+	public void setUserOnHomeScreen(boolean userOnHomeScreen)
+	{
+		this.userOnHomeScreen = userOnHomeScreen;
+	}
+
+	public boolean isGsrNotification()
+	{
+		return gsrNotification;
+	}
+
+	public void setGsrNotification(boolean gsrNotification)
+	{
+		this.gsrNotification = gsrNotification;
+	}
+
+	public boolean isHeartRateNotification()
+	{
+		return heartRateNotification;
+	}
+
+	public void setHeartRateNotification(boolean heartRateNotification)
+	{
+		this.heartRateNotification = heartRateNotification;
+	}
+
+	public boolean isAccelerometerNotification()
+	{
+		return accelerometerNotification;
+	}
+
+	public void setAccelerometerNotification(boolean accelerometerNotification)
+	{
+		this.accelerometerNotification = accelerometerNotification;
+	}
+
+	public boolean isTempratureNotification()
+	{
+		return tempratureNotification;
+	}
+
+	public void setTempratureNotification(boolean tempratureNotification)
+	{
+		this.tempratureNotification = tempratureNotification;
+	}
+
+	public boolean isGsrTextDisplay()
+	{
+		return gsrTextDisplay;
+	}
+
+	public void setGsrTextDisplay(boolean gsrTextDisplay)
+	{
+		this.gsrTextDisplay = gsrTextDisplay;
+	}
+
+	public boolean isHeartRateTextDisplay()
+	{
+		return heartRateTextDisplay;
+	}
+
+	public void setHeartRateTextDisplay(boolean heartRateTextDisplay)
+	{
+		this.heartRateTextDisplay = heartRateTextDisplay;
+	}
+
+	public boolean isAccelerometerTextDisplay()
+	{
+		return accelerometerTextDisplay;
+	}
+
+	public void setAccelerometerTextDisplay(boolean accelerometerTextDisplay)
+	{
+		this.accelerometerTextDisplay = accelerometerTextDisplay;
+	}
+
+	public boolean isTempratureTextDisplay()
+	{
+		return tempratureTextDisplay;
+	}
+
+	public void setTempratureTextDisplay(boolean tempratureTextDisplay)
+	{
+		this.tempratureTextDisplay = tempratureTextDisplay;
+	}
+
+	public boolean isGsrVisualDisplay()
+	{
+		return gsrVisualDisplay;
+	}
+
+	public void setGsrVisualDisplay(boolean gsrVisualDisplay)
+	{
+		this.gsrVisualDisplay = gsrVisualDisplay;
+	}
+
+	public boolean isHeartRateVisualDisplay()
+	{
+		return heartRateVisualDisplay;
+	}
+
+	public void setHeartRateVisualDisplay(boolean heartRateVisualDisplay)
+	{
+		this.heartRateVisualDisplay = heartRateVisualDisplay;
+	}
+
+	public boolean isAccelerometerVisualDisplay()
+	{
+		return accelerometerVisualDisplay;
+	}
+
+	public void setAccelerometerVisualDisplay(boolean accelerometerVisualDisplay)
+	{
+		this.accelerometerVisualDisplay = accelerometerVisualDisplay;
+	}
+
+	public boolean isTempratureVisualDisplay()
+	{
+		return tempratureVisualDisplay;
+	}
+
+	public void setTempratureVisualDisplay(boolean tempratureVisualDisplay)
+	{
+		this.tempratureVisualDisplay = tempratureVisualDisplay;
 	}
 
 	@Override
@@ -224,16 +360,15 @@ public class User implements Serializable, Populator
 				jsonObject = jArray.getJSONObject(i);
 				User user = new User();
 
-				user.Uid = objJson.getString("u_id");
-				user.Username = objJson.getString("firstname");
-				user.name = objJson.getString("firstname");
-				user.Gender = objJson.getString("gender");
-				user.DOB = objJson.getString("dob");
-				user.Emailid = objJson.getString("emailid");
-				user.Registrationdate = objJson.getString("registrationdate");
-				user.Height = objJson.getString("height");
-				user.Weight = objJson.getString("weight");
-				user.Devicid = objJson.getString("d_id");
+				user.uId = objJson.getString("u_id");
+				user.userName = objJson.getString("firstname");
+				user.gender = objJson.getString("gender");
+				user.dob = objJson.getString("dob");
+				user.eMailid = objJson.getString("emailid");
+				user.registrationdate = objJson.getString("registrationdate");
+				user.height = objJson.getString("height");
+				user.weight = objJson.getString("weight");
+				user.devicid = objJson.getString("d_id");
 				userList.add(user);
 			}
 		} catch (JSONException e)
